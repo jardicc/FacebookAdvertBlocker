0f3c2868806fb3ba012eff0647fc72fa00a801ef
jardicc <github.com@bereza.cz>
2017-06-21 22:47:13 +0200

    Removes sponsored posts

--- src/content.js
+++ src/content.js
@@ -1,4 +1,4 @@
-var elements, text, feedStream, filter = 'Navrhovaný příspěvek';
+var elements, text, feedStream, filter = 'Navrhovaný příspěvek', filter2='Sponzorováno';
 
 clearFeed();
 setInterval(clearFeed, 300);
@@ -10,11 +10,17 @@ function clearFeed()
 			feedStream = document.body.querySelector("[id^='feed_stream']");
 		}
 		elements = feedStream.querySelectorAll('span>span[class]');
-	    for (var i = elements.length-1; i > 0; i--){
+	    for (var i = elements.length-1; i >= 0; i--){
 	        if(filter.localeCompare(elements[i].innerText)===0){ //fastest way to compare strings?
 	            // uncomment for debug
 	            // elements[i].closest("[data-testid^='fbfeed']").style.outline = "5px solid black";
 	            // elements[i].closest("[data-testid^='fbfeed']").style.opacity = "0.1";
+	            elements[i].closest("[data-testid^='fbfeed']").style.display = "none";
+	        }
+	    }  
+	    elements = feedStream.querySelectorAll('h6+div>span>a[rel=nofollow]');
+	    for (var i = elements.length-1; i >= 0; i--){
+	        if(filter2.localeCompare(elements[i].innerText)===0){
 	            elements[i].closest("[data-testid^='fbfeed']").style.display = "none";
 	        }
 	    }  
